22db7fc9642a25d23b11a03e3095ac4a987bcf2b
torres <avicitwangyuan@163.com>
2019-04-03 22:55:41 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnGitDemo
+//
+//  Created by torres on 2019/4/3.
+//  Copyright © 2019 torres. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
